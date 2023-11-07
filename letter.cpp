da16a924616f68ddbd120aafa2e1d88fa94e7b56
Darrin Hubbard <darrinhubbard@Faded-Inc-WebUI--UX-Dept.local>
2023-11-07 10:35:38 -0600

    C++ program based on a letter to myself

--- letter.cpp
+++ letter.cpp
@@ -6,6 +6,7 @@ int main()
 std::cout << "Dear Self,\n";
 std::cout << "Let's focus this summer on getting our first animated short film project out to the public but first, we need to come up with our own in-house software since our current software is out of date and let's also find a job while we're at it.☺️ \n";
 std::cout << "\n";
+std::cout << "\n";
 std::cout << "Sincerely,\n"; 
 std::cout << "Darrin \n";  
 std::cout << "June 20th, 2022 \n";  
